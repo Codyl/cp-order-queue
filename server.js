b6f507b302ed49d71d7d3f954b2d938fba3744e5
Cody Lillywhite <codylillyw@gmail.com>
2020-06-27 23:59:43 -0400

    milestone1

--- server.js
+++ server.js
@@ -21,27 +21,27 @@ app.get('/full-pallets-list/', function(req, res) {
   //Get the full case version of the same page
   app.get('/full-cases-list/', function(req, res) {
     //create a query that uses the .sql file for the item orders information
-    const q = {
-      text: "SELECT * FROM itemsOrders",
+  //   const q = {
+  //     text: "SELECT * FROM itemsOrders",
       
-    }
-    //Try to execute that query
-    pool
-    .query(q)
-    .then(function(dbRes) {
-      console.log(dbRes.rows[0]+'test');
-      res.status(200).json(dbRes.rows[0]);
-    })
-    .catch(function(err) {
-      console.error("Error in query: ", err);
-      res.status(500).json({success: false, data: err});
-    });
-    res.render('pages/full-cases-list')
-  });
-  app.get('/partials-list/', function(req, res) {
-    res.render('pages/partials-list')
-  });
-  app.get('/view-only/', function(req, res) {
+  //   }
+  //   //Try to execute that query
+  //   pool
+  //   .query(q)
+  //   .then(function(dbRes) {
+  //     console.log(dbRes.rows[0]+'test');
+  //     res.status(200).json(dbRes.rows[0]);
+  //   })
+  //   .catch(function(err) {
+  //     console.error("Error in query: ", err);
+  //     res.status(500).json({success: false, data: err});
+  //   });
+  //   res.render('pages/full-cases-list')
+  // });
+  // app.get('/partials-list/', function(req, res) {
+  //   res.render('pages/partials-list')
+  // });
+  // app.get('/view-only/', function(req, res) {
 
   res.render('pages/view-only');
   });
