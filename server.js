b13793564e37d7cf7503b4d27dd6896dc4ecb81b
Cody Lillywhite <codylillyw@gmail.com>
2020-07-01 13:16:21 -0400

    update

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ var http = require('http');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-const { Pool, Client } = require('pg'); // https://node-postgres.com
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {rejectUnauthorized: false},
-});
+const { Pool } = require('pg'); // https://node-postgres.com
+const pool = new Pool();
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
@@ -17,31 +15,25 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get('/full-pallets-list/', function(req, res) {
   res.render('pages/full-pallets-list')
-  });
-  //Get the full case version of the same page
-  app.get('/full-cases-list/', function(req, res) {
-    //create a query that uses the .sql file for the item orders information
-  //   const q = {
-  //     text: "SELECT * FROM itemsOrders",
-      
-  //   }
-  //   //Try to execute that query
-  //   pool
-  //   .query(q)
-  //   .then(function(dbRes) {
-  //     console.log(dbRes.rows[0]+'test');
-  //     res.status(200).json(dbRes.rows[0]);
-  //   })
-  //   .catch(function(err) {
-  //     console.error("Error in query: ", err);
-  //     res.status(500).json({success: false, data: err});
-  //   });
-  //   res.render('pages/full-cases-list')
-  // });
-  // app.get('/partials-list/', function(req, res) {
-  //   res.render('pages/partials-list')
-  // });
-  // app.get('/view-only/', function(req, res) {
+});
 
-  res.render('pages/view-only');
+//Get the full case version of the same page
+app.get('/full-cases-list/', function(req, res) {
+  console.log("here");
+  //create a query that uses the .sql file for the item orders information
+  res.render('pages/full-cases-list');
+  //Try to execute that query
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    //pool.end()
+  })
+  
+});
+
+  app.get('/partials-list/', function(req, res) {
+    res.render('pages/partials-list');
+  });
+
+  app.get('/view-only/', function(req, res) {
+    res.render('pages/view-only');
   });
