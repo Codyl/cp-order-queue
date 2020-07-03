83eb952850e1d4a244e807507baf1e0212c20dad
Cody Lillywhite <codylillyw@gmail.com>
2020-07-03 18:09:33 -0400

    reverted to working state

--- server.js
+++ server.js
@@ -25,29 +25,34 @@ app.get('/full-pallets-list/', function(req, res) {
 app.get('/full-cases-list/', function(req, res) {
   
   //queries
-  let queryOrders = `select order_id FROM orders`;//Gets an array of orders
-  // let queryForRowInfo = `SELECT i.case_qty,i.name,inv.qty_avail,io.cs_qty,io.order_id,io.item_id
-  //     FROM itemsOrders io
-  //     JOIN items i ON io.item_id=i.item_id 
-  //     JOIN inventory inv ON inv.item_id=io.item_id AND inv.warehouse_id=1`;
-  // let queryPickBin = `SELECT b.name
-  //     FROM bins b
-  //     JOIN itemBins ib ON ib.bin_id=b.bin_id AND ib.warehouse_id=1
-  //     JOIN itemsOrders io ON io.item_id=ib.item_id
-  //     WHERE b.is_pick_bin=true ORDER BY io.items_orders_id`;
-  // let queryWBin = `SELECT b.name
-  //     FROM bins b 
-  //     JOIN itemBins ib ON ib.bin_id=b.bin_id AND ib.warehouse_id=1
-  //     JOIN itemsOrders io ON io.item_id=ib.item_id
-  //     WHERE b.is_pick_bin=false ORDER BY ib.quantity DESC`;
+  let queryOrders = "select * FROM orders";
+  let queryForRowInfo = `SELECT i.case_qty,i.name,inv.qty_avail,io.cs_qty,io.order_id,io.item_id
+      FROM itemsOrders io
+      JOIN items i ON io.item_id=i.item_id 
+      JOIN inventory inv ON inv.item_id=io.item_id AND inv.warehouse_id=1`;
+  let queryPickBin = `SELECT b.name
+      FROM bins b
+      JOIN itemBins ib ON ib.bin_id=b.bin_id AND ib.warehouse_id=1
+      JOIN itemsOrders io ON io.item_id=ib.item_id
+      WHERE b.is_pick_bin=true ORDER BY io.items_orders_id`;
+  let queryWBin = `SELECT b.name
+      FROM bins b 
+      JOIN itemBins ib ON ib.bin_id=b.bin_id AND ib.warehouse_id=1
+      JOIN itemsOrders io ON io.item_id=ib.item_id
+      WHERE b.is_pick_bin=false ORDER BY ib.quantity DESC`;
   
-  pool.query(queryOrders, function(err, dbRes) {
+  pool.query(queryForRowInfo, function(err, dbRes) {
     //console.log("query result:", err, dbRes.rows);
-    res.render('pages/full-cases-list.ejs', {orders: dbRes.rows});    
-  })
-    
+    pool.query(queryPickBin, function(err, dbRes2) {
+      pool.query(queryWBin, function(err, dbRes3) {
+        // res.locals.myData = dbRes.rows;
+        // res.locals.myPickBin = dbRes2.rows;
+        // res.locals.myWBin = dbRes3.rows;
+        res.render('pages/full-cases-list.ejs', {myData: dbRes.rows, myPickBin: dbRes2.rows, myWBin: dbRes3.rows });    
+      })
+    })
     // res.json(query.rows);
-  
+  })
 });
 
 app.get('/partials-list/', function(req, res) {
@@ -71,35 +76,5 @@ app.delete('/deleteOrder/:order_id', function(req, res) {
     // } else {
     //   res.status(400).send(dbErr);
     // }
-  });
-});
-
-//list generation
-app.get('primaryBinForItem/:item_id',function(req,res) {
-  let queryWBin = `SELECT b.name
-  FROM bins b 
-  JOIN itemBins ib ON ib.bin_id=b.bin_id AND ib.warehouse_id=1
-  WHERE b.is_pick_bin=false AND ib.item_id=$1::int`;
-  pool.query(queryPickBin, function(err, res) {
-    res.send(res.rows);//send primary bin name to fill span#itemName_Pri
-  });
-});
-app.get('pickBinForItem/:item_id',function(req,res) {
-  let queryPickBin = `SELECT b.name
-      FROM bins b
-      JOIN itemBins ib ON ib.bin_id=b.bin_id AND ib.warehouse_id=1
-      WHERE b.is_pick_bin=true AND ib.item_id=$1::int`;
-  pool.query(queryPickBin, function(err, res) {
-    res.send(res.rows);//send pick bin name to fill span#itemName_Pick
-  });
-});
-app.get('rowInfoByOrder/:order_id',function(req,res) {
-  let queryForRowInfo = `SELECT i.case_qty,i.name,inv.qty_avail,io.cs_qty,io.order_id,io.item_id
-      FROM itemsOrders io
-      JOIN items i ON io.item_id=i.item_id 
-      JOIN inventory inv ON inv.item_id=io.item_id AND inv.warehouse_id=1
-      WHERE i.item_id=$1::int AND io.order_id$2::int`;
-  pool.query(queryForRowInfo, function(err, res) {
-    res.send(res.rows);//send item/order details to fill all other td
   });
 });
